a49d7b5f232a35c7bb2d620d97d83fe9cf743e97
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-05-16 18:07:58 -0300

    Configurando o servidor SMTP do Gmail e corpo da mensagem para envio de mensagens e habilitando o acesso menos seguro ao Gmail.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EmporioVirtual.Models;
+using EmporioVirtual.Libraries.Email;
 
 namespace EmporioVirtual.Controllers
 {
@@ -31,10 +32,14 @@ namespace EmporioVirtual.Controllers
         [HttpPost]
         public IActionResult ContatoAcao()
         {
-            string nome = HttpContext.Request.Form["nome"];
-            string email = HttpContext.Request.Form["email"];
-            string texto = HttpContext.Request.Form["texto"];
-            return new ContentResult() { Content = $"Dados recebidos com sucesso!<br/> Nome: {nome} <br/> E-mail: {email} <br/>Texto: {texto}" , ContentType = "text/html"};
+            Contato contato = new Contato();
+
+            contato.Nome = HttpContext.Request.Form["nome"];
+            contato.Email = HttpContext.Request.Form["email"];
+            contato.Texto = HttpContext.Request.Form["texto"];
+
+            ContatoEmail.EnviarContatoPorEmail(contato); 
+            return new ContentResult() { Content = $"Dados recebidos com sucesso!<br/> Nome: {contato.Nome} <br/> E-mail: {contato.Email} <br/>Texto: {contato.Texto}" , ContentType = "text/html"};
         }
 
         public IActionResult Login()
---
+++ Libraries/Email/ContatoEmail.cs
@@ -0,0 +1,48 @@
+﻿using EmporioVirtual.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EmporioVirtual.Libraries.Email
+{
+    public class ContatoEmail
+    {
+        public static void EnviarContatoPorEmail (Contato contato)
+        {
+            /*
+                SMTP - Servidor que vai enviar a mensagem
+             */
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("boloteca.teste@gmail.com", "684@Fgvkab37_2");
+            smtp.EnableSsl = true;
+
+            string corpoMsg = string.Format("<h2>Contato - Empório Virtual</h2>"+
+                "<b>Nome: </b> {0} <br />"+
+                "<b>Email: </b> {1} <br />"+
+                "<b>Email: </b> {2} <br />"+
+                "<br /> E-mail enviado automaticamente do site EmporioVirtual",
+                contato.Nome,
+                contato.Email,
+                contato.Texto);
+            /*
+             * MailMessage -> contruir mensagem
+             */
+            MailMessage mensagem = new MailMessage();
+            mensagem.From = new MailAddress("boloteca.teste@gmail.com");
+            mensagem.To.Add("boloteca.teste@gmail.com");
+            mensagem.Subject = "Contato - Empório Virtual - E-mail: " + contato.Email;
+            mensagem.Body = corpoMsg;
+            mensagem.IsBodyHtml = true;
+
+            //enviar email
+            smtp.Send(mensagem);
+        }
+    }
+}
