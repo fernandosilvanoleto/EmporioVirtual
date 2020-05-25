26b704645d86bc60da2e10998c4f497d2a907c27
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-05-25 18:51:53 -0300

    Implementando injenção de dependência com criação de pasta como Repository fazendo o trabalho entre Model e Controller.

---
+++ Repositories/ClienteRepository.cs
@@ -0,0 +1,52 @@
+﻿using EmporioVirtual.Database;
+using EmporioVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmporioVirtual.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private EmporioVirtualContext _banco;
+        public ClienteRepository(EmporioVirtualContext banco)
+        {
+            _banco = banco;
+        }
+        public void Atualizar(Cliente cliente)
+        {
+            _banco.Update(cliente);
+            _banco.SaveChanges();
+        }
+
+        public void Cadastrar(Cliente cliente)
+        {
+            _banco.Add(cliente);
+            _banco.SaveChanges();
+        }
+
+        public void Excluir(int id)
+        {
+            Cliente cliente = ObterCliente(id);
+            _banco.Remove(cliente);
+            _banco.SaveChanges();
+        }
+
+        public Cliente Login(string Email, string Senha)
+        {
+            Cliente cliente = _banco.Clientes.Where(c => c.Email == Email && c.Senha == Senha).FirstOrDefault();
+            return cliente;
+        }
+
+        public Cliente ObterCliente(int id)
+        {
+            return _banco.Clientes.Find(id);
+        }
+
+        public List<Cliente> ObterTodosClientes()
+        {
+            return _banco.Clientes.ToList();
+        }
+    }
+}
---
+++ Repositories/IClienteRepository.cs
@@ -0,0 +1,20 @@
+﻿using EmporioVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmporioVirtual.Repositories
+{
+    interface IClienteRepository
+    {
+        Cliente Login(string Email, string Senha);
+        
+        void Cadastrar(Cliente cliente);
+        void Atualizar(Cliente cliente);
+        void Excluir(int id);
+        Cliente ObterCliente(int id);
+        List<Cliente> ObterTodosClientes();
+
+    }
+}
