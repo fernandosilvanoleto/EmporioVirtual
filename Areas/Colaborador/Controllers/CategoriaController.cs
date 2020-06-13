921f975eb5f687bae1bfe2d2c51fdfcd6c4d981f
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-06-13 14:07:44 -0300

    Implementando classe de Categoria em Area de Colaborador.

---
+++ Areas/Colaborador/Controllers/CategoriaController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmporioVirtual.Libraries.Filtro;
+using EmporioVirtual.Models;
+using EmporioVirtual.Repositories.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmporioVirtual.Areas.Colaborador.Controllers
+{
+    [Area("Colaborador")]
+    //TODO: habilitar verificação de login
+   // [ColaboradorAutorizacao]
+    public class CategoriaController : Controller
+    {
+        private ICategoriaRepository _categoriaRepository;
+
+        public CategoriaController(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+        public IActionResult Index()
+        {
+            List<Categoria> categorias = _categoriaRepository.ObterTodosCategorias().ToList();
+            return View(categorias);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar([FromForm]Categoria categoria )
+        {
+            //TODO: implementar
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Atualizar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Atualizar([FromForm]Categoria categoria)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int id)
+        {
+            return View();
+        }
+    }
+}
