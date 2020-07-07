2faf70417b02b19d597f5071ce96a917613faad1
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-07-07 18:58:50 -0300

    Implementando cadastro de imagens em produto - Parte 01

---
+++ Areas/Colaborador/Controllers/ImagemController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmporioVirtual.Libraries.Arquivo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmporioVirtual.Areas.Colaborador.Controllers
+{
+    [Area("Colaborador")]
+    public class ImagemController : Controller
+    {
+        public IActionResult Armazenar(IFormFile file)
+        {
+            GerenciadorArquivo.CadastrarImagemProduto(file);
+            return View();
+        }
+
+        public IActionResult Deletar()
+        {
+            return View();
+        }
+    }
+}
