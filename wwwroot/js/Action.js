b18a83b1fbd0d730cb0de76edfbb8baaabc998f5
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-07-09 19:41:17 -0300

    Implementando em Javascript para buscar arquivo em computador.

--- wwwroot/js/Action.js
+++ wwwroot/js/Action.js
@@ -8,3 +8,20 @@
     });
     $('.dinheiro').mask('000.000.000.000.000,00', { reverse: true });
 });
+
+$(document).ready(function () {
+    $(".img-upload").click(function () {
+        $(this).parent().find(".input-file").click();
+        //alert('oi');
+    });
+
+    $(".input-file").change(function () {
+        // FORMULÁRIO DE DADOS VIA JAVASCRIPT
+        var Binary = $(this)[0].file[0]; // campo selecionado
+        var formulario = new FormData();
+        formulario.append("file", Binary);
+
+        //TODO -Requisição Ajax enviando o formulário para model
+
+    });
+});
