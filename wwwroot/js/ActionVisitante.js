7569bb5ef0225b7b7fb8abf5ce814c00a88a8c60
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-07-30 18:48:40 -0300

    Javascript para ordenação de produtos - Parte 02

---
+++ wwwroot/js/ActionVisitante.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $("#ordenacao").change(function () {
+        //TODO - Redirecionar para a tela Home/Index passando as QueryString de Ordenação e mantendo a Página de pesquisa
+        var Pagina = 1;
+        var Pesquisa = "";
+        var Ordenacao = $(this).val();
+
+        var QueryString = new URLSearchParams(window.location.search);
+        if (QueryString.has("pagina")) {
+            Pagina = QueryString.get("pagina");
+        }
+
+        if (QueryString.has("pesquisa")) {
+            Pesquisa = QueryString.get("pesquisa");
+        }
+
+        // gerar a url
+        // não precisa, mas deixa ela aqui
+        var URL = window.location.hostname + "//" + window.location.port + window.location.pathname;
+
+        var URLComparametros = "?pagina=" + Pagina + "&pesquisa=" + Pesquisa + "&ordenacao=" + Ordenacao + "#ordenacao";
+
+        //alert(URLComparametros);
+
+        window.location.href = URLComparametros;
+
+    });
+});
