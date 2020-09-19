49fa389fb631e2209a0aed264c0d8366173c5358
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-09-19 18:11:51 -0300

    Criação da biblioteca CalculaPacote e do Model Pacote - Web Correios

---
+++ Libraries/Gerenciador/Frete/CalcularPacote.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmporioVirtual.Models;
+using EmporioVirtual.Models.ProdutoAgregador;
+
+namespace EmporioVirtual.Libraries.Gerenciador.Frete
+{
+    public class CalcularPacote
+    {
+        //RECEBER UMA LISTA DE PRODUTOS PARA CALCULAR FRETE
+        public List<Pacote> CalcularPacotesDeProduto(List<Produto> produtos)
+        {
+            List<Pacote> pacotes = new List<Pacote>();
+
+            //pacotes.Add(pacotes);
+
+            return pacotes;
+        }
+    }
+}
