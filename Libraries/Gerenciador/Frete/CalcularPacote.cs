2f7a33f47bfba474db96e375664cd7d54f5a7f4c
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-09-20 09:32:50 -0300

    Implementando código de cálculo frete - Parte 01

--- Libraries/Gerenciador/Frete/CalcularPacote.cs
+++ Libraries/Gerenciador/Frete/CalcularPacote.cs
@@ -10,9 +10,49 @@ namespace EmporioVirtual.Libraries.Gerenciador.Frete
     public class CalcularPacote
     {
         //RECEBER UMA LISTA DE PRODUTOS PARA CALCULAR FRETE
-        public List<Pacote> CalcularPacotesDeProduto(List<Produto> produtos)
+        public List<Pacote> CalcularPacotesDeProduto(List<ProdutoItem> produtos)
         {
             List<Pacote> pacotes = new List<Pacote>();
+
+            Pacote pacote = new Pacote();
+
+            // VER PRODUTO POR PRODUTO
+            foreach (var item in produtos)
+            {
+
+                // CRIAR A DIMENSÃO DO PACOTE
+                for (int i = 0; i < item.QuantidadeProdutoCarrinho; i++)
+                {
+                    var peso = pacote.Peso = item.Peso;
+
+                    //ADOTAR O COMPRIMENTO E A LARGURA DO MAIOR PRODUTO
+                    var comprimento = (pacote.Comprimento > item.Comprimento) ? pacote.Comprimento : item.Comprimento;
+                    var largura = (pacote.Largura > item.Largura) ? pacote.Largura : item.Largura;
+
+                    var altura = pacote.Altura + item.Altura;
+
+                    // DIMENSÃO DO PRODUTO
+                    var dimensao_produto = comprimento + largura + altura;
+
+                    // REGRAS DOS CORREIOS
+                    //CRIAR NOVOS PACOTES:
+                    // SE O PESO > 30kg, DIMENSÃO > 200 cm = CRIAR NOVO PACOTE
+
+                    if (peso > 30 || dimensao_produto > 200)
+                    {
+                        pacotes.Add(pacote);
+                        pacote = new Pacote();
+                    }
+
+
+                    // UM NOVO PACOTE
+                    pacote.Peso = pacote.Peso + item.Peso;
+                    pacote.Comprimento = (pacote.Comprimento > item.Comprimento) ? pacote.Comprimento : item.Comprimento;
+                    pacote.Largura = (pacote.Largura > item.Largura) ? pacote.Largura : item.Largura;
+
+                    pacote.Altura = pacote.Altura + item.Altura;
+                }
+            }
 
             //pacotes.Add(pacotes);
 
